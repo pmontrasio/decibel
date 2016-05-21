9d5bf674876289eb88bba2ba745ea644c2221c9e
Paolo Montrasio <paolo@paolomontrasio.com>
2016-05-21 16:35:04 +0200

    Settings for MacAddress

---
+++ apps/mac_address/config/dev.exs
@@ -0,0 +1,4 @@
+import Mix.Config
+
+config :mac_address, tshark_cmd: "/home/montra/phoenix-tests/beacon/channel_monitor wlo1" # systemd interface name
+
---
+++ apps/mac_address/config/prod.exs
@@ -0,0 +1,4 @@
+import Mix.Config
+
+config :mac_address, tshark_cmd: "/home/montra/phoenix-tests/beacon/channel_monitor wlan1" # pre-systemd interface name
+
